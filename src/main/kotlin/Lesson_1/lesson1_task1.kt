61dbb8b6c657ef7fb43dd913e977fe2fd027cccf
LuciaCari <konlud@mail.ru>
2025-04-26 16:30:19 +0300

    KS-1-1 refactor code

--- src/main/kotlin/Lesson_1/lesson1_task1.kt
+++ src/main/kotlin/Lesson_1/lesson1_task1.kt
@@ -1,6 +1,6 @@
 package org.example.Lesson_1
 
-fun main(){
-    var NumberOfOrders: Int = 75
-    val GratitudeText : String = "Благодарим за покупку в нашем интернет-магазине!"
+fun main() {
+    val numberOfOrders: Int = 75
+    val gratitudeText: String = "Благодарим за покупку в нашем интернет-магазине!"
 }
