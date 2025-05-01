c63abcc5d26c97bbeb9b4b80aace0f7d080fea0c
LuciaCari <konlud@mail.ru>
2025-05-01 23:42:18 +0300

    KS-1-5 converting seconds to hours

---
+++ src/main/kotlin/Lesson_1/lesson1_task5.kt
@@ -0,0 +1,25 @@
+package org.example.Lesson_1
+
+fun main() {
+//    Объяви переменную с количеством секунд, которые Гагарин провел в космосе.
+//    - Переведи в коде количество секунд в целые минуты и целые часы,
+//    сохраняя остаток секунд и минут в новые переменные;
+//    - Выведи в консоль время, проведенное в космосе в формате [часы:минуты:секунды],
+//    например так: 01:30:09;
+//    - Значения часов, минут и секунд отображать двумя цифрами.
+
+    val flightInSeconds: Short = 6480
+
+    // определяем целое количество часов и остаток в секундах
+    val flightInHours: Int = flightInSeconds / (SEC_IN_MINUTE * MIN_IN_HOUR)
+    var remainderInSeconds: Int = flightInSeconds % (SEC_IN_MINUTE * MIN_IN_HOUR)
+
+    // определяем целое количество минут и остаток в секундах
+    val flightInMinutes: Int = remainderInSeconds / SEC_IN_MINUTE
+    remainderInSeconds = remainderInSeconds % SEC_IN_MINUTE
+
+    println("${"%02d".format(flightInHours)}:${"%02d".format(flightInMinutes)}:${"%02d".format(remainderInSeconds)}")
+}
+
+const val SEC_IN_MINUTE: Byte = 60
+const val MIN_IN_HOUR: Byte = 60
