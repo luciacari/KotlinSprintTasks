b2b79dc047678d6dde4f49da710b5d1b9f1c6854
LuciaCari <konlud@mail.ru>
2025-05-02 15:10:27 +0300

    KS-5-4 refactot access to the spaceship

--- src/main/kotlin/Lesson_5/lesson5_task4.kt
+++ src/main/kotlin/Lesson_5/lesson5_task4.kt
@@ -10,16 +10,17 @@ fun main() {
     println("Введи, пожалуйста, свое имя пользователя:")
     enteredUserName = readln()
 
-    if (enteredUserName != userName)
+    if (enteredUserName == userName) {
+        println("Введите, пожалуйста, пароль для пользователя \"$enteredUserName\":")
+        enteredUserPassword = readln()
+        if (enteredUserPassword == userPassword) {
+            println("$userName, добро пожаловать в систему!")
+        } else println("Введённый пароль неверен. Доступ запрещён!")
+
+    } else {
         println(
             "Извините, пользователь с именем \"$enteredUserName\" не зарегистрирован в системе!\n" +
                     "Хотите зарегистрироваться?"
         )
-    else println("Введите, пожалуйста, пароль для пользователя \"$enteredUserName\":")
-    enteredUserPassword = readln()
-    if (enteredUserPassword == userPassword)
-        println("$userName, добро пожаловать в систему!")
-    else println("Введённый пароль неверен. Доступ запрещён!")
-
-
+    }
 }
